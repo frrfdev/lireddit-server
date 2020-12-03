cb9ee073b920fe50557ce5dbc6fc2301e8736875
frrfdev <feliperenanrf23@gmail.com>
2020-12-03 13:43:38 -0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import mikroConfig from "./mikro-orm.config";
+import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { PORT } from "./constants";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+const main = async () => {
+  const conn = await MikroORM.init(mikroConfig);
+  await conn.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: conn.em,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    return res.send("hello");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`server started on localhost:${PORT}`);
+  });
+};
+
+main();
---
+++ src/resolvers/user.ts
@@ -0,0 +1,148 @@
+import { MyContext } from "src/types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Query(() => User, { nullable: true })
+  user(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<User | null> {
+    return em.findOne(User, { id });
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (!options.username)
+      return {
+        errors: [
+          { message: "The username cannot be empty", field: "username" },
+        ],
+      };
+
+    if (!options.password)
+      return {
+        errors: [
+          { message: "The username cannot be empty", field: "password" },
+        ],
+      };
+
+    const hashedPassword = await argon2.hash(options.password);
+
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      // duplicated error
+      if (error.code === "23505" || error.detail.includes("already exists")) {
+        return {
+          errors: [{ message: "username already taken", field: "username" }],
+        };
+      }
+    }
+
+    return { user };
+  }
+
+  @Query(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user)
+      return {
+        errors: [
+          { field: "username", message: "that username doesn't exists" },
+        ],
+      };
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid)
+      return {
+        errors: [{ field: "password", message: "incorrect password" }],
+      };
+
+    return { user };
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async updateUser(
+    @Arg("username") username: string,
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<User | null> {
+    const user = await em.findOne(User, { id });
+
+    if (!user) return null;
+
+    if (typeof username !== undefined) {
+      user.username = username;
+      await em.persistAndFlush(user);
+    }
+
+    return user;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteUser(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    await em.nativeDelete(User, { id });
+
+    return true;
+  }
+}
