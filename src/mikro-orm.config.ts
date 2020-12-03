250711c4aec26454c0df5b42d8fa88a37745f323
frrfdev <feliperenanrf23@gmail.com>
2020-12-03 14:01:42 -0300

    Database info

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,18 +1,18 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { DB_NAME, DB_PASSWORD, DB_USER, __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import path from 'path';
+import path from "path";
 import { User } from "./entities/User";
 
 export default {
   migrations: {
-    path: path.join(__dirname, './migrations'),
+    path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [Post, User],
-  dbName: "lireddit",
-  type: 'postgresql',
+  dbName: DB_NAME,
+  type: "postgresql",
   debug: !__prod__,
-  user: 'postgres',
-  password: '1234'
+  user: DB_USER,
+  password: DB_PASSWORD,
 } as Parameters<typeof MikroORM.init>[0];
