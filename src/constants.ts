4936f83da84f3ed1db13320fce375ffbb28d98b3
frrfdev <feliperenanrf23@gmail.com>
2020-12-03 14:23:20 -0300

    database host

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ export const PORT = 4000;
 export const DB_NAME = __prod__ ? process.env.DATABASE_NAME : "lireddit";
 export const DB_USER = __prod__ ? process.env.DATABASE_USER : "postgres";
 export const DB_PASSWORD = __prod__ ? process.env.DATABASE_PASSWORD : "1234";
+export const DB_HOST = __prod__ ? process.env.DATABASE_HOST : "127.0.0.1";
