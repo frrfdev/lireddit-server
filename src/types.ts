ba9d3f63cdd7a12d67dcbd900d787426fa603ffa
frrfdev <feliperenanrf23@gmail.com>
2020-12-04 00:57:48 -0300

    alter declare modules file

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,6 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 import { Request, Response } from "express";
 
-declare module "express-session" {
-  interface Session {
-    userId: number;
-  }
-}
-
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
   req: Request;
