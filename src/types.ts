9675e4436d27891fa97a828b77f8429bfd988421
frrfdev <feliperenanrf23@gmail.com>
2020-12-04 00:00:51 -0300

    session userId

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 
+declare module "express-session" {
+  interface Session {
+    userId: number;
+  }
+}
+
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
 }
