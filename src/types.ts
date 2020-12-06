a96ed4daaea7f3083bfec7f70429cf0751f4e42c
frrfdev <feliperenanrf23@gmail.com>
2020-12-06 14:39:14 -0300

    deploy to heroku

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,8 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 import * as Express from "express";
 
-// declare module "express-session" {
-//   interface Session {
-//     userId: number;
-//   }
-// }
-
 export interface MyContext {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Express.Request;
+  req: Express.Request & { session: { userId: number } };
   res: Express.Response;
 }
